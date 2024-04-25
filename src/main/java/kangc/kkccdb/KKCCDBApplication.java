a24e7176ac93a140eecce619b202804ff97e1219
kang.cao <1457857389@qq.com>
2024-04-25 22:55:51 +0800

    初始项目

---
+++ src/main/java/kangc/kkccdb/KKCCDBApplication.java
@@ -0,0 +1,10 @@
+package kangc.kkccdb;
+
+/**
+ * 动手写个数据库！启动
+ */
+public class KKCCDBApplication {
+    public static void main(String[] args) {
+        System.out.println("hello, kkccdb");
+    }
+}
